62b93f03b15ef092b5e527c35676d124d4dbcc29
saint734 <christianovalle820@gmail.com>
2025-10-31 23:18:33 +0900

    new function for stats and new print statement for main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from stats import get_book_text
 from stats import word_counter
+from stats import char_counter
 
 word_count = word_counter(get_book_text("books/frankenstein.txt"))
 print(f'Found {word_count} total words')
+dict_count = char_counter(get_book_text("books/frankenstein.txt"))
+print(dict_count)
 
--- stats.py
+++ stats.py
@@ -7,3 +7,9 @@ def word_counter(book:str) -> int:
     word_count = len(book.split())
     return word_count
 
+def char_counter(book:str) -> dict:
+    counts = {}
+
+    for i in range(len(book)):
+        counts[book[i].lower()] = 1 + counts.get(book[i].lower(),0)
+    return counts
