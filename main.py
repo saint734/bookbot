92ac63f0353c84cbd62f9160b9b96968ab5512cf
saint734 <christianovalle820@gmail.com>
2025-11-01 20:08:25 +0900

    new function for stats and changed main output plus added new books

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from stats import get_book_text
 from stats import word_counter
 from stats import char_counter
+from stats import dict_sort
+import sys 
 
-word_count = word_counter(get_book_text("books/frankenstein.txt"))
-print(f'Found {word_count} total words')
-dict_count = char_counter(get_book_text("books/frankenstein.txt"))
-print(dict_count)
+filepath = sys.argv
+print("=" * 12, "BOOKBOT", "=" * 12)
+try: 
+    print(f"Analyzing book found at {filepath[1]}...")
+except IndexError as e:
+    print("Missing a second argument")
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+print("-" * 10, "Word Count", "-" * 11)
 
+word_count = word_counter(get_book_text(filepath[1]))
+print(f"Found {word_count} total words")
+print("-" * 8, "Character Count", "-" * 8)
+
+dict_count = char_counter(get_book_text(filepath[1]))
+#print(dict_count)
+#sorted_dict = dict_sort(dict_count)
+print(dict_sort(dict_count))
+print("=" * 14, "END", "=" * 14)
--- stats.py
+++ stats.py
@@ -1,4 +1,6 @@
-def get_book_text(filepath: str) -> str:
+import io
+
+def get_book_text(filepath:str) -> str:
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
@@ -11,5 +13,13 @@ def char_counter(book:str) -> dict:
     counts = {}
 
     for i in range(len(book)):
-        counts[book[i].lower()] = 1 + counts.get(book[i].lower(),0)
+        if book[i]:
+            counts[book[i].lower()] = 1 + counts.get(book[i].lower(),0)
     return counts
+
+def dict_sort(characters:dict) -> list:
+    sorted_dict = dict(sorted(characters.items(), key=lambda item: item[1], reverse=True))
+    # Got help here: https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
+    for i in sorted_dict:
+        if i.isalpha():
+            print(f"{i}: {sorted_dict[i]}")
